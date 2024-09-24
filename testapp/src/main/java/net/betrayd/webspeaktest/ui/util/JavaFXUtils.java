e22ae6c341dfc6e06ae56c1195b80037a56b8190
Sam54123 <thesam54123@gmail.com>
2024-09-24 13:36:28 -0700

    added significant QOL features to testapp

--- testapp/src/main/java/net/betrayd/webspeaktest/ui/util/JavaFXUtils.java
+++ testapp/src/main/java/net/betrayd/webspeaktest/ui/util/JavaFXUtils.java
@@ -2,8 +2,15 @@ package net.betrayd.webspeaktest.ui.util;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.collect.MapMaker;
 
+import javafx.beans.binding.IntegerBinding;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -15,6 +22,8 @@ public class JavaFXUtils {
     private static record ColorSizeKey(Color lineColor, Double size) {};
 
     private static Map<ColorSizeKey, Image> gridImages = new MapMaker().weakValues().makeMap();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JavaFXUtils.class);
 
     public static ImagePattern createGridPattern(double gridSize, double x, double y, Color lineColor) {
         double w = gridSize;
@@ -38,5 +47,36 @@ public class JavaFXUtils {
 
     public static ImagePattern createGridPattern(double gridSize, double x, double y) {
         return createGridPattern(gridSize, x, y, Color.BLACK);
+    }
+
+    public static IntegerBinding stringToIntBinding(ObservableValue<? extends String> source) {
+        return new IntegerBinding() {
+
+            {
+                super.bind(source);
+            }
+
+            @Override
+            public ObservableList<?> getDependencies() {
+                return FXCollections.singletonObservableList(source);
+            }
+
+            @Override
+            public void dispose() {
+                super.dispose();
+                super.unbind(source);
+            }
+
+            @Override
+            protected int computeValue() {
+                try {
+                    return Integer.valueOf(source.getValue());
+                } catch (Exception e) {
+                    LOGGER.warn("NumberFormatException: {}", e.getMessage());
+                    return 0;
+                }
+            }
+            
+        };
     }
 }
