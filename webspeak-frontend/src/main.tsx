c5ed496c8cf2dead6ff9e323e4664cc6b84ecdc3
Sam54123 <thesam54123@gmail.com>
2024-10-01 16:35:29 -0700

    got it deploying to github pages

--- webspeak-frontend/src/main.tsx
+++ webspeak-frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import 'bootswatch/dist/flatly/bootstrap.min.css'
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
