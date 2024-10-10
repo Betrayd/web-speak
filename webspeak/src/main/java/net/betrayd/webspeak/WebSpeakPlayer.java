6b73012f902743f13985cdd52d34a5ad02e76f8a
Sam54123 <thesam54123@gmail.com>
2024-10-10 16:08:07 -0700

    made player group code more robust

--- webspeak/src/main/java/net/betrayd/webspeak/WebSpeakPlayer.java
+++ webspeak/src/main/java/net/betrayd/webspeak/WebSpeakPlayer.java
@@ -3,7 +3,10 @@ package net.betrayd.webspeak;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.WeakHashMap;
 import java.util.function.Consumer;
 
 import org.slf4j.Logger;
@@ -56,7 +59,7 @@ public abstract class WebSpeakPlayer {
 
     private WebSpeakChannel channel = null;
 
-    private List<WebSpeakGroup> groups = new ArrayList<>();
+    private final List<WebSpeakGroup> groups = new ArrayList<>();
 
     /**
      * Return an unmodifiable list of groups the player is in. Groups with a greater
@@ -82,8 +85,9 @@ public abstract class WebSpeakPlayer {
             return false;
         }
         groups.add(group);
-        group.onAddPlayer(this);
         onAddGroup(group);
+        // Call this here because onAddGroup may need to be called without invalidating modifiers.
+        onInvalidateAudioModifiers(group.getAudioModifiedPlayers());
         return true;
     }
     
@@ -98,23 +102,126 @@ public abstract class WebSpeakPlayer {
             return false;
         }
         groups.add(index, group);
-        group.onAddPlayer(this);
         onAddGroup(group);
+        onInvalidateAudioModifiers(group.getAudioModifiedPlayers());
         return true;
+    }
+
+    /**
+     * Add a this player to a collection of groups.
+     * @param groups Groups to add.
+     */
+    public synchronized void addGroups(Collection<? extends WebSpeakGroup> groups) {
+        Set<WebSpeakGroup> groupSet = new HashSet<>(this.groups);
+        Set<WebSpeakPlayer> invalidPlayers = new HashSet<>();
+        for (var group : groups) {
+            // Easy way to check for duplicates
+            if (groupSet.add(group)) {
+                this.groups.add(group);
+                onAddGroup(group);
+                invalidPlayers.addAll(group.getAudioModifiedPlayers());
+            }
+        }
+        onInvalidateAudioModifiers(invalidPlayers);
     }
 
     /**
      * Remove this player from a group.
      * @param group Group to remove from.
-     * @return
+     * @return If the player was in the group.
      */
     public synchronized boolean removeGroup(WebSpeakGroup group) {
+        Collection<WebSpeakPlayer> invalidPlayers = null;
+        boolean success = false;
         if (groups.remove(group)) {
-            group.onRemovePlayer(this);
+            // This is called before onRemoveGroup so, in case the group has an audio
+            // modifier on itself, this player gets included in that.
+            invalidPlayers = group.getAudioModifiedPlayers();
             onRemoveGroup(group);
-            return true;
+            success = true;
         }
-        return false;
+        if (success) {
+            onInvalidateAudioModifiers(invalidPlayers);
+        }
+        return success;
+    }
+
+    /**
+     * Remove this player from a collection of groups.
+     * @param groups Groups to remove from.
+     * @return If the player was in any of the groups.
+     */
+    public synchronized boolean removeGroups(Collection<? extends WebSpeakGroup> groups) {
+        Set<WebSpeakPlayer> invalidPlayers = new HashSet<>();
+        // Checks make sure this will never duplicate.
+        boolean success = false;
+        var iterator = this.groups.iterator();
+        while (iterator.hasNext()) {
+            var group = iterator.next();
+            if (groups.contains(group)) {
+                iterator.remove();
+                invalidPlayers.addAll(group.getAudioModifiedPlayers());
+                onRemoveGroup(group);
+                success = true;
+            }
+        }
+
+        onInvalidateAudioModifiers(invalidPlayers);
+        return success;
+    }
+    
+    /**
+     * Arbitrarily modify the group list for this player, and call the appropriate
+     * callbacks based on what was changed.
+     * 
+     * @param modifier Function to modify the groups.
+     * @throws IllegalArgumentException If the modifier function adds duplicate groups.
+     * @implNote Because of the amount of checks & listeners needed, this method is
+     *           fairly inefficient. Use the other group methods for simpler tasks.
+     */
+    public synchronized void modifyGroups(Consumer<? super List<WebSpeakGroup>> modifier) throws IllegalArgumentException {
+        List<WebSpeakGroup> prev = List.copyOf(groups);
+        List<WebSpeakGroup> modified = new ArrayList<>(groups);
+        modifier.accept(modified);
+
+        // Ths could probably be more efficient, but it's called rarely with a small dataset so it doesn't really matter.
+        Set<WebSpeakGroup> duplicateChecker = new HashSet<>();
+        for (var item : modified) {
+            if (!duplicateChecker.add(item)) {
+                throw new IllegalArgumentException("Player may not have two instances of a group. (" + item + ")");
+            }
+        }
+
+        List<WebSpeakGroup> removedGroups = new ArrayList<>(modified.size());
+        List<WebSpeakGroup> addedGroups = new ArrayList<>(modified.size());
+
+        for (var group : modified) {
+            if (!prev.contains(group)) {
+                addedGroups.add(group);
+            }
+        }
+
+        for (var group : prev) {
+            if (!modified.contains(group)) {
+                removedGroups.add(group);
+            }
+        }
+
+        groups.clear();
+        groups.addAll(modified);
+
+        // Don't invalidate audio modifier for players twice.
+        Set<WebSpeakPlayer> invalidModifiers = new HashSet<>();
+        for (var added : addedGroups) {
+            onAddGroup(added);
+            invalidModifiers.addAll(added.getAudioModifiedPlayers());
+        }
+        for (var removed : removedGroups) {
+            onRemoveGroup(removed);
+            invalidModifiers.addAll(removed.getAudioModifiedPlayers());
+        }
+        
+        onInvalidateAudioModifiers(invalidModifiers);
     }
 
     private void onInvalidateAudioModifiers(Collection<? extends WebSpeakPlayer> players) {
@@ -130,8 +237,8 @@ public abstract class WebSpeakPlayer {
      * @param group Group that was added to.
      */
     protected void onAddGroup(WebSpeakGroup group) {
+        group.onAddPlayer(this);
         group.ON_MODIFIER_INVALIDATED.addListener(invalidateAudioModifiersListener);
-        onInvalidateAudioModifiers(group.getAudioModifiedPlayers());
     }
 
     /**
@@ -139,9 +246,14 @@ public abstract class WebSpeakPlayer {
      * @param group Group that was removed from.
      */
     protected void onRemoveGroup(WebSpeakGroup group) {
+        group.onRemovePlayer(this);
         group.ON_MODIFIER_INVALIDATED.removeListener(invalidateAudioModifiersListener);
-        onInvalidateAudioModifiers(group.getAudioModifiedPlayers());
     }
+
+    /**
+     * Cache the audio modifiers sent to the player so we don't resend. Make sure to clear on left scope.
+     */
+    private final WeakHashMap<WebSpeakPlayer, AudioModifier> audioModifierCache = new WeakHashMap<>();
     
     /**
      * Re-calculate the audio modifiers used on a given player and send to the client.
@@ -152,15 +264,40 @@ public abstract class WebSpeakPlayer {
             return;
         
         AudioModifier modifier = computeAudioModifier(player);
-        new SetAudioModifierS2CPacket(playerId, modifier).send(wsContext);
+        if (!modifier.equals(audioModifierCache.get(player))) {
+            new SetAudioModifierS2CPacket(playerId, modifier).send(wsContext);
+            audioModifierCache.put(player, modifier);
+        }
     }
-
-    protected synchronized AudioModifier computeAudioModifier(WebSpeakPlayer player) {
+    
+    /**
+     * Compute the audio modifier this player will use for a target player based on
+     * both players' groups.
+     * 
+     * @param player Target player.
+     * @return The combined audio modifier.
+     * @implNote This method does not modify or rely on the cache in any way. It
+     *           always computes the audio modifier from scratch and returns it,
+     *           regardless of scope.
+     */
+    public synchronized AudioModifier computeAudioModifier(WebSpeakPlayer player) {
         AudioModifier modifier = AudioModifier.DEFAULT;
         for (var group : groups) {
             modifier = AudioModifier.combine(group.computeAudioModifier(player), modifier);
         }
         return modifier;
+    }
+    
+    /**
+     * Get the audio modifier this player has cached for another player without
+     * recalculating. Will only be valid if players are in scope.
+     * 
+     * @param player Target player.
+     * @return The audio modifier, or <code>null</code> if the players aren't in
+     *         scope.
+     */
+    public synchronized AudioModifier getCachedAudioModifier(WebSpeakPlayer player) {
+        return audioModifierCache.get(player);
     }
 
     public final WebSpeakChannel getChannel() {
@@ -196,10 +333,13 @@ public abstract class WebSpeakPlayer {
     }
 
     /**
-     * Called when this player has left the scope of another player.
+     * Called when this player has left the scope of another player. Called for all
+     * players in scope when this player's client disconnects.
+     * 
      * @param other The other player.
      */
     protected void onLeftScope(WebSpeakPlayer other) {
+        audioModifierCache.remove(other);
     }
 
 
